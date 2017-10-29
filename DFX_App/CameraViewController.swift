ec5534efd618c0fdc74df1286f29ce95b8487f72
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-10-29 00:34:05 -0400

    Version with camera using camera controller

---
+++ DFX_App/CameraViewController.swift
@@ -0,0 +1,116 @@
+//
+//  CameraViewController.swift
+//  DFX_App
+//
+//  Created by Nathan Lewis on 10/28/17.
+//  Copyright © 2017 Nathan Lewis. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class CameraViewController: UIViewController {
+    
+    let cameraController = CameraController()
+
+    @IBOutlet weak var capturePreviewView: UIView!
+    
+    @IBOutlet weak var toggleFlashButton: UIButton!
+    
+    @IBOutlet weak var toggleCameraButton: UIButton!
+    
+    @IBOutlet weak var captureButton: UIButton!
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        func configureCameraController() {
+            cameraController.prepare {(error) in
+                if let error = error {
+                    print(error)
+                }
+                
+                try? self.cameraController.displayPreview(on: self.capturePreviewView)
+            }
+        }
+        
+        func styleCaptureButton() {
+            captureButton.layer.borderColor = UIColor.black.cgColor
+            captureButton.layer.borderWidth = 2
+            
+            captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+        }
+        
+        styleCaptureButton()
+        
+        configureCameraController()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func toggleFlash(_ sender: Any) {
+        if cameraController.flashMode == .on {
+            cameraController.flashMode = .off
+            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash Off Icon"), for: .normal)
+        }
+            
+        else {
+            cameraController.flashMode = .on
+            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash On Icon"), for: .normal)
+        }
+    }
+    
+    
+    @IBAction func switchCameras(_ sender: Any) {
+        do {
+            try cameraController.switchCameras()
+        }
+            
+        catch {
+            print(error)
+        }
+        
+        switch cameraController.currentCameraPosition {
+        case .some(.front):
+            toggleCameraButton.setImage(#imageLiteral(resourceName: "Front Camera Icon"), for: .normal)
+            
+        case .some(.rear):
+            toggleCameraButton.setImage(#imageLiteral(resourceName: "Rear Camera Icon"), for: .normal)
+            
+        case .none:
+            return
+        }
+    }
+    
+    
+    @IBAction func captureImage(_ sender: Any) {
+        cameraController.captureImage {(image, error) in
+            guard let image = image else {
+                print(error ?? "Image capture error")
+                return
+            }
+            
+            try? PHPhotoLibrary.shared().performChangesAndWait {
+                PHAssetChangeRequest.creationRequestForAsset(from: image)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
